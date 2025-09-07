16170f1ee76c7e0e6a15d96c3c7449b86ce8c21f
Stanislav Svoboda <52444330+nuclearkofola@users.noreply.github.com>
2025-09-07 16:39:48 +0200

    starter point

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p >
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
